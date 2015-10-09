0873a9f2f7463d005b706b0119c5ed0e3d2828b2
Nickolay Kuropatkin <nictaporuk@yandex.ru>
2015-10-09 11:19:49 +0300

    add links to require js

--- js/app.js
+++ js/app.js
@@ -4,11 +4,24 @@
 console.log('init app.js');
 
 require.config({
-// пары "ключ-значение" вашего приложения
-    baseUrl: "app",
-// Обычно тот же каталог, в котором находится сценарий верхнего уровня,
-// указанный в атрибуте data-main
-    paths: {},
-// set up custom paths to libraries, or paths to RequireJS plug-ins
-    shim: {}, // используется для настройки адаптеров (подробнее см. ниже)
+    // пары "ключ-значение" вашего приложения
+    baseUrl: "js/app",
+    // Обычно тот же каталог, в котором находится сценарий верхнего уровня,
+    // указанный в атрибуте data-main
+    paths: {
+        'underscore'    : '../bower_components/underscore/underscore-min.js',
+        'backbone'      : '../bower_components/backbone/backbone',
+        'backbone'      : '../bower_components/jquery/dist/jquery.min.js'
+    },
+    // set up custom paths to libraries, or paths to RequireJS plug-ins
+    // используется для настройки адаптеров
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        }
+    }
 });
